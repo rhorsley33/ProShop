3895ef23ebeccf9da49c22b09c16991bc818e221
Robby <rmhorsley27@gmail.com>
2024-04-15 23:10:05 -0600

    set up express server - get products from backend rather than hardcoded file in frontend directory

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import products from '../products'
+import axios from 'axios'
 import Rating from '../components/Rating'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 const ProductScreen = () => {
+    const [product, setProduct] = useState([]);
     const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId);
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        };
+        fetchProduct();
+    }, [productId]);
   return (
     <>
         <Link className='btn btn-light my-3' to='/'>
